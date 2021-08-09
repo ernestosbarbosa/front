321ddd1adad74254cedf8de7f27e27766e89e196
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2021-08-09 12:25:44 -0300

    Updates

---
+++ precache-manifest.a1ddcc0bef1807345022c7a38586f5e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b198562659c3434b33da8e5fe91b24a",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "4b442f343a2c13095de9",
+    "url": "/front/static/css/2.0012cb62.chunk.css"
+  },
+  {
+    "revision": "22eb7fc0e59521b735dd",
+    "url": "/front/static/css/main.cfccbc7b.chunk.css"
+  },
+  {
+    "revision": "4b442f343a2c13095de9",
+    "url": "/front/static/js/2.c8d2ce50.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/front/static/js/2.c8d2ce50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eb7fc0e59521b735dd",
+    "url": "/front/static/js/main.16806d0d.chunk.js"
+  },
+  {
+    "revision": "1b1ff31f3bf3e34b581b",
+    "url": "/front/static/js/runtime-main.feb71eff.js"
+  },
+  {
+    "revision": "532833bab5c10a5de60a6dc3db88ddb1",
+    "url": "/front/static/media/serverestlogo1.532833ba.png"
+  },
+  {
+    "revision": "7e020988ad25abee813d0c6a56a66723",
+    "url": "/front/static/media/serverestlogo1branco.7e020988.png"
+  },
+  {
+    "revision": "f431ee71be27736de068da4c0423b587",
+    "url": "/front/static/media/trabalho.f431ee71.png"
+  }
+]);
