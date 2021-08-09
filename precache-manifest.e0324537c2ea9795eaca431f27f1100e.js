0811383482b312a0ac8f7a3fd458a5df4c1b9699
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2021-08-09 14:47:12 -0300

    Updates

---
+++ precache-manifest.e0324537c2ea9795eaca431f27f1100e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b277036d3f06d6e31eb5588591ccb21",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "4b442f343a2c13095de9",
+    "url": "/front/static/css/2.0012cb62.chunk.css"
+  },
+  {
+    "revision": "5f997707880cb4152c15",
+    "url": "/front/static/css/main.cfccbc7b.chunk.css"
+  },
+  {
+    "revision": "4b442f343a2c13095de9",
+    "url": "/front/static/js/2.c8d2ce50.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/front/static/js/2.c8d2ce50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f997707880cb4152c15",
+    "url": "/front/static/js/main.aa3342a9.chunk.js"
+  },
+  {
+    "revision": "1b1ff31f3bf3e34b581b",
+    "url": "/front/static/js/runtime-main.feb71eff.js"
+  },
+  {
+    "revision": "532833bab5c10a5de60a6dc3db88ddb1",
+    "url": "/front/static/media/serverestlogo1.532833ba.png"
+  },
+  {
+    "revision": "7e020988ad25abee813d0c6a56a66723",
+    "url": "/front/static/media/serverestlogo1branco.7e020988.png"
+  },
+  {
+    "revision": "f431ee71be27736de068da4c0423b587",
+    "url": "/front/static/media/trabalho.f431ee71.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.827411a193662501a364b7a588ec1398.js"
+  "/front/precache-manifest.e0324537c2ea9795eaca431f27f1100e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
